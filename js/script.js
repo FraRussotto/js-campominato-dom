cecfb0a471f51462f754f384a787b55135a77d37
FraRussotto <russottofrancesco1@gmail.com>
2023-09-22 00:17:09 +0200

    [fix] bombs + endGame

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-const main = document.querySelector('.game_wrapper');
+//1. Dopo aver creato la griglia, al click di play dobbiamo generare le bombe.
+
+//2. Cliccando le celle dobbiamo scoprire se è una bomba o meno.
+
+//3. Facendo riferimento a positionBombs, creo una funzione che mi ritorni una array con gli indici delle celle che saranno bombe.
+
+//4. Modifico l'eventListener della cell, inserendo il controllo sulla presenza della bomba, e quindi terminare il gioco, o se andare avanti aumentando il puntaggio.
+
+//5. Una volta stabilite le opzioni al click della cella, creo la funzione che finalizza il gioco a seconda dell'esito del click.
+
+
+const main = document.getElementById('game_wrapper');
 const playBtn = document.querySelector('#play')
-
 const levelSelected = document.querySelector('#level');
-
 const levels = [100, 81, 49]
 let numberSquare;
 
+let positionBombs = [];
+const numBombs = 16;
+
+let points = 0;
 
 playBtn.addEventListener('click', play);
 
-let positionBomb = [];
 
 // ----- Function ----- //
 
@@ -17,67 +29,77 @@ function play(){
   numberSquare = levels[levelSelected.value]
   reset();
   generatePlayGrid();
+  positionBombs = generateBomb();
 }
 
 function generatePlayGrid(){
-  const grid = document.createElement('div');
-  grid.classList.add('grid')
 
-  const positionBomb = randomListBomb(numberSquare);
-  console.log(positionBomb);
+  const grid = document.createElement('div');
+  grid.classList.add('grid') 
 
   for(let i = 0; i < numberSquare; i++) {
     const square = createSquare(i);
-    if(positionBomb.includes(i)){
-      square.classList.add('bomb')
-    } 
-    console.log(square)
     grid.append(square)
   }
-
   main.append(grid)
 }
 
 function createSquare(index){
-  const square =  document.createElement('div');
+  const square = document.createElement('div');
   square.className = 'square';
   square.classList.add('square' + numberSquare);
   square._squareID = index;
   square.addEventListener('click', hendlerClickSquare)
-
   return square;
 }
 
+//4.
+
 function hendlerClickSquare(){
 
-  let flag = true;
-  let point = 0;
-  do{
-    if(this.classList.contains('bomb')){
-      console.log('hai perso')
-      flag = false;
-    }else{
-      this.classList.add('clicked');
-      this.removeEventListener('click', hendlerClickSquare);
-      point++;
-    }
-
+  if(positionBombs.includes(this._squareID)) endGame(false);
+  else {
+    this.classList.add('clicked');
+    points++;
+    if(points === numberSquare - numBombs) endGame(true);
   }
-  while(flag === false)
+  this.removeEventListener('click', hendlerClickSquare);
+
 }
+
+//3.
+
+function generateBomb(){
+  const bombs = [];
+  while(bombs.length < numBombs){
+    const bomb = Math.ceil(Math.random() * numberSquare);
+    if(!bombs.includes(bomb)) bombs.push(bomb);
+  }
+  console.log(bombs);
+  return bombs;
+}
+
+//5.
+function endGame(isWin){
+  revealBombs();
+  //7. A fine gioco, "blocco" il gioco tramite un elemento creato e "appeso" al game_wrapper.
+  const stop = document.createElement('div');
+  stop.className = 'stop';
+  main.append(stop);
+}
+
+//6. Creo una funzione da utilizzare in endGame che mi fa visualizzare tutte le bombe, ciclando tutte le celle e verificando se il loro index è contenuto in positionBombs.
+
+function revealBombs(){
+  const squares = document.getElementsByClassName('square')
+  for(let i = 0; i < squares.length; i++){
+    const square = squares[i];
+    if(positionBombs.includes(square._squareID)) square.classList.add('bomb')
+  }
+}
+
 
 function reset(){
   main.innerHTML = '';
   positionBomb = [];
-}
-
-function randomListBomb(index){
-  while(positionBomb.length < 16){
-    let randomNum = Math.ceil(Math.random() * index);
-    if(!positionBomb.includes(randomNum)) {
-      positionBomb.push(randomNum);
-    } 
-
-  }
-  return positionBomb;
 }
